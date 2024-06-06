a64fee1d0e9cb9e72e4f89abcc6495db124982fc
Eliete <eli3tec@gmail.com>
2024-06-06 20:57:47 -0300

    Exemplo sem uso do Encapsulamento

---
+++ exemplosPilaresPOO/src/Autodromo.java
@@ -0,0 +1,8 @@
+public class Autodromo {
+    public static void main(String[] args) {
+        Carro Jeep = new Carro();
+        Jeep.confereCambio();
+        Jeep.confereCombustivel();
+        Jeep.ligar();
+    }
+}
---
+++ exemplosPilaresPOO/src/Carro.java
@@ -0,0 +1,13 @@
+public class Carro{
+    public void ligar(){
+        System.out.println("Carro ligado");
+    }
+
+    public void confereCombustivel(){
+        System.out.println("Conferindo combustivel");
+    }
+
+    public void confereCambio(){
+        System.out.println("Conferindo cambio em P");
+    }
+}
