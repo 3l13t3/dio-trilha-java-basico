18830cb59274befda7f3194939cc30654e3db2ff
Eliete <eli3tec@gmail.com>
2024-06-06 21:48:58 -0300

    Heranca de recursos comum

---
+++ exemploHerancaPOO/src/Autodromo.java
@@ -0,0 +1,11 @@
+public class Autodromo {
+
+    public static void main(String[] args) {
+        Carro Jeep = new Carro();
+        Jeep.setChassi("789346");
+        Jeep.ligar();
+
+        Moto Versys300 = new Moto();
+        Versys300.setChassi("062025");
+    }
+}
---
+++ exemploHerancaPOO/src/Carro.java
@@ -0,0 +1,16 @@
+public class Carro extends Veiculo{
+    
+    public  void ligar() {
+        conferirCambio();
+        conferirCombustivel();
+        System.out.println("Carro ligado");
+    }
+
+    private void conferirCombustivel() {
+        System.out.println("Conferindo combustivel");
+    }
+
+    private void conferirCambio() {
+        System.out.println("Conferindo cambio em P");
+    }
+}
