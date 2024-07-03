60f5e18a1c0c0a514447db46eee60ff53fac5c9e
Eliete <eli3tec@gmail.com>
2024-07-03 12:42:30 -0300

    Correcao da main no arquivo XML

--- DesafioDioBanco/src/main/java/com/eli3tec/desafiodiobanco/Banco.java
+++ DesafioDioBanco/src/main/java/com/eli3tec/desafiodiobanco/Banco.java
@@ -11,26 +11,24 @@ package com.eli3tec.desafiodiobanco;
 import java.util.List;
 
 public class Banco {
-	
-	private String nome;
-	private List<Conta> contas;
-	
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public List<Conta> getContas(){
-		return contas;
-	}
-	
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
-	
+
+    private String nome;
+    private List<Conta> contas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
 
 }
